39fe8c815a1ed24c1430acde0e257f279c355dcf
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-04-16 17:05:24 +0700

    update policy path

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { PolicyComponent } from './policy/policy.component';
+import { PolicyComponent } from './components/policy/policy.component';
 
 @NgModule({
   declarations: [
