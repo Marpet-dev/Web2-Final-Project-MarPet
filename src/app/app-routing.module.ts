9848860a9c42442f20c29f92a31868cd0db3aa6e
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-04-18 13:53:35 +0700

    thêm app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { CatComponent } from './components/cat/cat.component';
+import { DogComponent } from './components/dog/dog.component';
+import { HomeComponent } from './components/home/home.component';
 import { NewDetailComponent } from './components/new-detail/new-detail.component';
 import { NewsComponent } from './components/news/news.component';
+import { PetServicesComponent } from './components/pet-services/pet-services.component';
+import { PolicyComponent } from './components/policy/policy.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path: 'news', component: NewsComponent},
-  {path: 'news/:id',component: NewDetailComponent}
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'cat', 
+    component: CatComponent
+  },
+  {
+    path: 'dog', 
+    component: DogComponent
+  },
+  {
+    path: 'home', 
+    component: HomeComponent
+  },
+  {
+    path: 'news', 
+    component: NewsComponent
+  },
+  {
+    path: 'news/:id', 
+    component: NewDetailComponent
+  },
+  {
+    path: 'pet-services', 
+    component: PetServicesComponent
+  },
+  {
+    path: 'policy', 
+    component: PolicyComponent
+  },
+  {
+    path: 'sign-in', 
+    component: SignInComponent
+  },
+  {
+    path: 'sign-up', 
+    component: SignUpComponent
+  }
 ];
 
 @NgModule({
