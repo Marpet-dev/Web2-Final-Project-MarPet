5d46c84dbc95631263b2d5f33335183830be5b29
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-04-19 19:49:09 +0700

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project'),
+      dir: require('path').join(__dirname, './coverage/web2-final-project-marpet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
