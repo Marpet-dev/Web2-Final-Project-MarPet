7c20d57bcb55a6ddf3b93995cecf8596f63a142a
Đinh Hoàng Duy <93324589+DinhDuyhihi@users.noreply.github.com>
2022-05-20 12:47:34 +0700

    add validation check

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { customValidator, passwordValidator } from 'src/app/validators/check.validators';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  public regForm:any;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.regForm = this._formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3), customValidator(/\@|\#|\$|\%|\^|\&/g)]],
+      email: ['', Validators.email],
+      pass: ['', Validators.required, Validators.minLength(8)],
+      confirmPass: ['', Validators.required]
+    }, {Validators: [passwordValidator]})
   }
-
+  get username(){
+    return this.regForm.controls['username']
+  }
 }
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { customValidator, passwordValidator } from 'src/app/validators/check.validators';
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'app-sign-in',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public regForm:any;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.regForm = this._formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3), customValidator(/\@|\#|\$|\%|\^|\&/g)]],
+      email: ['', Validators.email],
+      pass: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPass: ['', [Validators.required, Validators.minLength(8)]],
+      phonenumber:['', [Validators.required, Validators.pattern("^((\\+84-?)|0)?[0-9]{9}$")]]
+    }, {
+      validators: passwordValidator
+    })
   }
-
+  get f(){
+    return this.regForm.controls
+  }
+  // MustMatch(controlName: string, matchingControlName:string){
+  //   return(formGroup: FormGroup)=>{
+  //     const control = formGroup.controls[controlName];
+  //     const matchingControl = formGroup.controls[matchingControlName];
+  //     if(matchingControl.errors && !matchingControl.errors.MustMatch){
+  //       return
+  //     }
+  //     if(control.value !== matchingControl.value){
+  //       matchingControl.setErrors({MustMatch:true})
+  //     }
+  //     else{
+  //       matchingControl.setErrors(null)
+  //     }
+  //   }
+  // }
 }
